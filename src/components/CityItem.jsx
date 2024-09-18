2638bcfc9ca6f2903efb664409b05e33e5020765
Doan Huu Linh <doanlinh1510@gmail.com>
2024-09-19 01:37:18 +0700

    commit 7 Reading and Setting a Query String

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -18,14 +18,18 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
+  console.log(position);
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
-      <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
       </Link>
     </li>
   );
