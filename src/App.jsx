35023729543b4b00efa060008cddadc10008d7da
Doan Huu Linh <doanlinh1510@gmail.com>
2024-10-10 23:44:39 +0700

    commit 25 Optimizing Bundle Size With lazy() & Suspense of React

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import { lazy, Suspense } from "react";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Login from "./pages/Login";
-import HomePage from "./pages/HomePage";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Login from "./pages/Login";
+// import HomePage from "./pages/HomePage";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+// dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB :(
+// dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB :(
+
+const HomePage = lazy(()=>import("./pages/HomePage"))
+const Product = lazy(()=>import("./pages/Product"))
+const Pricing = lazy(()=>import("./pages/Pricing"))
+const Login = lazy(()=>import("./pages/Login"))
+const AppLayout = lazy(()=>import("./pages/AppLayout"))
+const PageNotFound = lazy(()=>import("./pages/PageNotFound"))
+
+
+
 
 export default function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage/>}>
           <Routes>
             <Route index element={<HomePage />} />
             <Route path="product" element={<Product />} />
@@ -40,6 +57,7 @@ export default function App() {
             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
+        </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
